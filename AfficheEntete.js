a1caabd84fb76f9b9afadfebc44827f9106f3c20
Bertolen <33809187+Bertolen@users.noreply.github.com>
2019-10-13 12:03:26 +0200

    Tout plein de changements
    Un entête sur chaque page
    Notes de campagne
    Informations sur les Grands Esprits du monde de Ferae

---
+++ AfficheEntete.js
@@ -0,0 +1,56 @@
+// Récupération de la racine du site
+function getBaseUrl() {
+    return window.location.href.match(/^.*\/\/[\w.:]*\//);
+}
+
+// Liste des liens
+var onglets = [
+    {
+        libele: "Campagnes",
+        lien: "Campagne/Accueil.html"
+    },
+    {
+        libele: "Apocalypse",
+        lien: "Apocalypse/LoupGarouApocalypse.html"
+    },
+    {
+        libele: "Déchus",
+        lien: "Dechus/LoupGarouDechus.html"
+    },
+    {
+        libele: "Ferae",
+        lien: "Ferae/Accueil.html"
+    }
+];
+
+var tableElt = document.createElement("table"); // Création du tableau
+
+// Création de la ligne du tableau
+var trElt = document.createElement("tr");
+
+// Pour chaque onglet a faire
+
+onglets.forEach(function(onglet){
+    
+    // Création de la case
+    var tdElt = document.createElement("td");
+
+    // Création du lien
+    var aElt = document.createElement("a");
+
+    // Alimentation du lien
+    aElt.textContent = onglet.libele;
+    aElt.href = getBaseUrl() + onglet.lien;
+
+    // Ajout du lien au tableau
+    tdElt.appendChild(aElt);
+
+    // Ajout de la case à la ligne
+    trElt.appendChild(tdElt);
+    
+});
+
+// On ajoute au tableau la ligne
+tableElt.appendChild(trElt);
+
+document.body.appendChild(tableElt); // Ajout de la liste à la page
